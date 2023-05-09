0240d0878b88d62310a626079273f25478081a46
Anika-Enosis <anika.tabassum@enosis.com>
2023-05-09 13:12:20 +0600

    initail commit

---
+++ Exercise/Controllers/HTMLController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Exercise.Controllers
+{
+    public class HTMLController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Introduction()
+        {
+            return View();
+        }
+
+        public IActionResult Elements()
+        {
+            return View();
+        }
+
+        public IActionResult Tags()
+        {
+            return View();
+        }
+
+        public IActionResult Heading()
+        {
+            return View();
+        }
+
+        public IActionResult OnlineEditor()
+        {
+            return View();
+        }
+
+        public IActionResult Paragraph()
+        {
+            return View();
+        }
+    }
+}
---
+++ Exercise/wwwroot/js/HTML/OnlineEditor.js
@@ -0,0 +1,24 @@
+﻿
+$(document).ready(function () {
+   ace.require("ace/ext/language_tools");
+   var editor = ace.edit("editor");
+   editor.setOptions({
+      enableBasicAutocompletion: true,
+      enableSnippets: true,
+      enableLiveAutocompletion: true
+   });
+
+   editor.getSession().setMode("ace/mode/html")
+   editor.setTheme("ace/theme/xcode")
+
+   $(document).on("click", "#run", function () {
+      var code = editor.getValue();
+      $("#output").html(code);
+   });
+
+   $(document).on("click", "#clear", function () {
+      editor.setValue("", 0);
+      $("#output").html("");
+   });
+   alert(isBanglaSelected);
+});
